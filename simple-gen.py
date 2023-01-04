25d9b1703df0c0da5756c1827d2fa62b2b6bbb49
Natan Facchin <n.facchin@sig.eu>
2023-01-04 21:17:41 +0100

    add base proj

---
+++ simple-gen.py
@@ -0,0 +1,23 @@
+# make sure you're logged in with `huggingface-cli login`
+import uuid
+from diffusers import StableDiffusionPipeline
+
+pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+pipe = pipe.to("mps")
+
+# Recommended if your computer has < 64 GB of RAM
+pipe.enable_attention_slicing()
+
+prompt = "a photo of a cute dog"
+
+# First-time "warmup" pass (see explanation above)
+_ = pipe(prompt, num_inference_steps=1)
+
+# Results match those from the CPU device after the warmup pass.
+image = pipe(prompt).images[0]
+
+image_name = str(uuid.uuid1()) + ".jpg"
+
+image.save(image_name)
+
+print("Done!")
