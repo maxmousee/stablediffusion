d4b9c622422b05fb206fb0723033f989ee852037
Natan Facchin <maxmousee@gmail.com>
2023-01-05 19:51:45 +0100

    Update simple-gen.py

--- simple-gen.py
+++ simple-gen.py
@@ -14,7 +14,7 @@ pipe = pipe.to("mps")
 # Recommended if your computer has < 64 GB of RAM
 pipe.enable_attention_slicing()
 
-prompt = "a photo of a cute dog"
+prompt = "a sketch of a cute dog"
 
 # First-time "warmup" pass (see explanation above)
 _ = pipe(prompt, num_inference_steps=1)
