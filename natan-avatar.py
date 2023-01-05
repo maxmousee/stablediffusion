2c119ca891e5b20ffc0198f590aaf72c46a93994
Natan Facchin <maxmousee@gmail.com>
2023-01-05 19:18:39 +0100

    add my own script, clean up code

--- natan-avatar.py
+++ natan-avatar.py
@@ -14,7 +14,7 @@ pipe = pipe.to("mps")
 # Recommended if your computer has < 64 GB of RAM
 pipe.enable_attention_slicing()
 
-prompt = "a photo of a cute dog"
+prompt = "a sketch of a man drinking whisky in a bar, black and white, 4k, 8k"
 
 # First-time "warmup" pass (see explanation above)
 _ = pipe(prompt, num_inference_steps=1)
